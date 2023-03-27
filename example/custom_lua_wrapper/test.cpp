8df09f1477ea00bd30adf060a5a35e3673230166
Shuangquan Li <lishq991@gmail.com>
2023-03-27 12:01:28 +0800

    add example for custom_lua_wrapper

--- example/custom_lua_wrapper/test.cpp
+++ example/custom_lua_wrapper/test.cpp
@@ -21,25 +21,23 @@
 struct provider {
   provider() { puts("provider()"); }
   ~provider() { puts("~provider()"); }
-  void provide(const std::vector<std::string> &vars, peacalm::lua_wrapper *l) {
-    for (const auto &v : vars) provide(v, l);
-  }
-  void provide(const std::string &v, peacalm::lua_wrapper *l) {
-    if (v == "a")
-      l->set_integer(v, 1);
-    else if (v == "b")
-      l->set_integer(v, 2);
-    else if (v == "c")
-      l->set_integer(v, 3);
+  bool provide(lua_State *L, const char *vname) {
+    if (strcmp(vname, "a") == 0)
+      lua_pushinteger(L, 1);
+    else if (strcmp(vname, "b") == 0)
+      lua_pushinteger(L, 2);
+    else if (strcmp(vname, "c") == 0)
+      lua_pushinteger(L, 3);
     else
-      std::cout << "unknown: " << v << std::endl;
+      return false;
+    return true;
   }
 };
 using provider_type = std::unique_ptr<provider>;
 
 int main() {
-  peacalm::lua_wrapper_has_provider<provider_type> l;
+  peacalm::custom_lua_wrapper<provider_type> l;
   l.provider(std::make_unique<provider>());
-  double ret = l.auto_eval_double("return a*10 + b^c");
-  std::cout << ret << std::endl;
+  double ret = l.eval_double("return a*10 + b^c");
+  std::cout << ret << std::endl;  // 18
 }
