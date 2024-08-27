fd200bd4b35557044bae3e03287dd3a42843216f
Shuangquan Li <lishq991@gmail.com>
2024-08-28 01:39:56 +0800

    Update example bind_class

--- example/bind_class/test.cpp
+++ example/bind_class/test.cpp
@@ -45,8 +45,11 @@ int main() {
   l.register_ctor<ConstObj(int, int)>(
       "NewConstObj2");  // constructor with 2 argument
 
+  // Register members
   l.register_member("i", &Obj::i);
   l.register_member("ci", &Obj::ci);  // const member
+
+  // Register member functions
   l.register_member("abs", &Obj::abs);
 
   // Register overloaded member functions.
@@ -59,12 +62,48 @@ int main() {
     l.log_error_out();
     return 1;
   }
+
   int absi = l.get_int("absi");
   Obj o    = l.get<Obj>("o");
+  assert(absi == 99);
+  assert(o.i == -99);
 
-  std::cout << "absi = " << absi << std::endl;
-  std::cout << "o.i = " << o.i << std::endl;
-  std::cout << "o.ci = " << o.ci << std::endl;
+  retcode = l.dostring("o.ci = 2");
+  assert(retcode != LUA_OK);
+  l.log_error_out();
+  // Lua: [string "o.ci = 2"]:1: Const member cannot be modified: ci
+  assert(o.ci == 1);
+
+  Obj o2;
+  l.set("o2", &o2);
+  l.dostring("o2:plusby(100)");
+  assert(l.get_int({"o2", "i"}) == 101);
+  assert(l.eval_int("return o2.i") == 101);
+  assert(o2.i == 101);
+
+  o2.i = 22;
+  assert(l.get<int>({"o2", "i"}) == 22);
+
+  retcode = l.dostring("co = NewConstObj2(-1, -2)");
+  assert(retcode == LUA_OK);
+  assert(l.eval_int("return co:abs()") == 1);
+  assert(l.eval_int("return co.i") == -1);
+  assert(l.eval_int("return co.ci") == -2);
+
+  retcode = l.dostring("co:plus()");
+  assert(retcode != LUA_OK);
+  l.log_error_out();
+  // Lua: [string "co:plus()"]:1: Nonconst member function: plus
+
+  bool failed = false;
+  int  coi    = l.eval_int("co.i = 1; return co.i",
+                       /* def */ 0,
+                       /* disable_log */ false,
+                       &failed);
+  // Lua: [string "co.i = 1; return co.i"]:1: Const member cannot be modified: i
+
+  assert(coi == 0);
+  assert(failed);
 
   return 0;
 }
