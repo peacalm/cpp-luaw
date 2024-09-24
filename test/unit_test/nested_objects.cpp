340fef27ff8d9946c6f756588cc94d5e425b9301
Li Shuangquan <lishq991@gmail.com>
2024-09-25 00:22:02 +0800

    test case nested_objects: add struct definition for volatile version

--- test/unit_test/nested_objects.cpp
+++ test/unit_test/nested_objects.cpp
@@ -16,6 +16,70 @@
 
 namespace {
 
+#if PEACALM_LUAW_SUPPORT_VOLATILE_OBJECT
+
+// Struct definitions for volatile.
+// Make the operator= return void to mute GNU compilation warning:
+// implicit dereference will not access object of type ‘volatile xxx’ ...
+
+struct A {
+  int i;
+
+  A() : i(1) {}
+  A(const volatile A& r) : i(r.i) {}
+  A(const volatile A&& r) : i(r.i) {}
+
+  void operator=(const volatile A& r) volatile { i = r.i; }
+  void operator=(const volatile A&& r) volatile { i = r.i; }
+};
+
+struct Ac {
+  const int ci;
+
+  Ac() : ci(1) {}
+  Ac(const volatile Ac& r) : ci(r.ci) {}
+  Ac(const volatile Ac&& r) : ci(r.ci) {}
+
+  void operator=(const volatile Ac& r) volatile  = delete;
+  void operator=(const volatile Ac&& r) volatile = delete;
+};
+
+struct B {
+  int i;
+  A   a;
+
+  B() : i(2) {}
+  B(const volatile B& r) : i(r.i), a(r.a) {}
+  B(const volatile B&& r) : i(r.i), a(std::move(r.a)) {}
+
+  void operator=(const volatile B& r) volatile {
+    i = r.i;
+    a = r.a;
+  }
+  void operator=(const volatile B&& r) volatile {
+    i = r.i;
+    a = r.a;
+  }
+};
+
+struct Bac {
+  Ac ac;
+
+  Bac() {}
+  Bac(const volatile Bac& r) : ac(r.ac) {}
+  Bac(const volatile Bac&& r) : ac(std::move(r.ac)) {}
+};
+
+struct Bcac {
+  const Ac cac;
+
+  Bcac() {}
+  Bcac(const volatile Bcac& r) : cac(r.cac) {}
+  Bcac(const volatile Bcac&& r) : cac(std::move(r.cac)) {}
+};
+
+#else
+
 struct A {
   int i = 1;
 };
@@ -37,6 +101,8 @@ struct Bcac {
   const Ac cac;
 };
 
+#endif
+
 TEST(nested_objects, register_nested_members) {
   peacalm::luaw l;
   l.register_member("i", &A::i);
@@ -45,10 +111,14 @@ TEST(nested_objects, register_nested_members) {
   l.register_member("a", &B::a);
   l.register_member<const A(B::*)>("a_as_c", &B::a);
 
-  // l.register_member("ac", &Bac::ac); // Error: no operator=
-  l.register_member<const Ac(Bac::*)>("ac_as_c", &Bac::ac);  // OK: set ac const
+  // Error: no operator=
+  // l.register_member("ac", &Bac::ac);  // compilation fail
 
-  l.register_member("cac", &Bcac::cac);  // OK cac already const
+  // OK: set ac as const
+  l.register_member<const Ac(Bac::*)>("ac_as_c", &Bac::ac);
+
+  // OK cac already const
+  l.register_member("cac", &Bcac::cac);
 }
 
 TEST(nested_objects, register_solid_nested_member) {
@@ -60,6 +130,7 @@ TEST(nested_objects, register_solid_nested_member) {
   auto b = std::make_shared<B>();
   l.set("b", b);
 
+  EXPECT_EQ(b->i, 2);
   EXPECT_EQ(l.eval<int>("return b.i"), b->i);
   EXPECT_TRUE(l.dostring("b.i = 5") == LUA_OK);
   EXPECT_TRUE(l.dostring("assert(b.i == 5)") == LUA_OK);
@@ -69,7 +140,7 @@ TEST(nested_objects, register_solid_nested_member) {
   // can't modify b's a
   b->a.i           = 1;
   const int oldbai = b->a.i;
-  EXPECT_EQ(l.eval<int>("return b.a.i"), b->a.i);
+  EXPECT_EQ(l.eval<int>("return b.a.i"), oldbai);
   EXPECT_TRUE(l.dostring("b.a.i = 5") == LUA_OK);
   EXPECT_TRUE(l.dostring("assert(b.a.i ~= 5);") == LUA_OK);
   EXPECT_EQ(l.eval<int>("return b.a.i"), oldbai);
@@ -81,6 +152,43 @@ TEST(nested_objects, register_solid_nested_member) {
   EXPECT_EQ(l.dostring("a.i = 123"), LUA_OK);
   EXPECT_EQ(l.eval<int>("return a.i"), 123);       // changed
   EXPECT_EQ(l.eval<int>("return b.a.i"), oldbai);  // no change
+}
+
+TEST(nested_objects, register_solid_nested_member_for_const) {
+  peacalm::luaw l;
+  l.register_member("i", &A::i);
+  l.register_member("i", &B::i);
+  l.register_member("a", &B::a);  // solid member a, which can not be modified
+
+  auto b = std::make_shared<const B>();
+  l.set("b", b);
+
+  EXPECT_EQ(b->i, 2);
+  EXPECT_EQ(l.eval<int>("return b.i"), b->i);
+  EXPECT_TRUE(l.dostring("b.i = 5") != LUA_OK);
+  l.log_error_out();
+  EXPECT_TRUE(l.dostring("assert(b.i ~= 5)") == LUA_OK);
+  EXPECT_EQ(l.eval<int>("return b.i"), b->i);
+  EXPECT_EQ(b->i, 2);
+
+  // can't modify b's a
+  // b->a.i = 1; // Error
+  const int oldbai = b->a.i;
+  EXPECT_EQ(l.eval<int>("return b.a.i"), oldbai);
+  EXPECT_TRUE(l.dostring("b.a.i = 5") != LUA_OK);
+  l.log_error_out();
+  EXPECT_TRUE(l.dostring("assert(b.a.i ~= 5);") == LUA_OK);
+  EXPECT_EQ(l.eval<int>("return b.a.i"), oldbai);
+  EXPECT_EQ(b->a.i, oldbai);
+
+  // get a copy of b'a, can't modify the copy's i too!
+  EXPECT_EQ(l.dostring("a = b.a"), LUA_OK);
+  EXPECT_EQ(l.eval<int>("return a.i"), oldbai);
+  EXPECT_TRUE(l.dostring("a.i = 123") != LUA_OK);
+  l.log_error_out();
+  EXPECT_NE(l.eval<int>("return a.i"), 123);
+  EXPECT_EQ(l.eval<int>("return a.i"), oldbai);
+  EXPECT_EQ(l.eval<int>("return b.a.i"), oldbai);
 }
 
 TEST(nested_objects, register_member_address) {
@@ -191,6 +299,8 @@ TEST(nested_objects, register_member_address) {
     EXPECT_EQ(l.dostring("b.aptr.i = 4"), LUA_OK);
     EXPECT_EQ(l.eval<int>("return b.aptr.i"), 4);
     EXPECT_EQ(l.eval<int>("return b.acptr.i"), 4);
+    EXPECT_EQ(b->a.i, 4);
+    EXPECT_EQ(p->i, 4);
 
     // can't modify by acptr
     EXPECT_NE(l.dostring("b.acptr.i = 5"), LUA_OK);
@@ -198,12 +308,15 @@ TEST(nested_objects, register_member_address) {
   }
 }
 
+#if !PEACALM_LUAW_SUPPORT_VOLATILE_OBJECT
+
 TEST(nested_objects, register_member_by_fake_shared_ptr) {
   peacalm::luaw l;
   l.register_member("i", &A::i);
   l.register_member("i", &B::i);
   l.register_member("a", &B::a);
 
+  // Compilation fail if support volatile B.
   l.register_member<std::shared_ptr<A> B::*>("aref", [](const B* p) {
     // a fake shared_ptr, whose deleter does nothing
     return std::shared_ptr<A>(&const_cast<B*>(p)->a, [](...) {});
@@ -233,123 +346,46 @@ TEST(nested_objects, register_member_by_fake_shared_ptr) {
   }
 }
 
-#if PEACALM_LUAW_SUPPORT_VOLATILE_OBJECT
+#endif
 
 struct Bval {
-  Bval() {}
+  int i;
+  A   a;
 
-  // copy ctor
-  Bval(const volatile Bval& r) {
-    i   = r.i;
-    a.i = r.a.i;
-  }
-
-  // move ctor
-  Bval(volatile Bval&& r) {
-    i   = r.i;
-    a.i = r.a.i;
-  }
-
-  // copy by const rvalue ref ???
-  Bval(const volatile Bval&& r) {
-    i   = r.i;
-    a.i = r.a.i;
-  }
-
-  ~Bval() {}
-
-  // volatile operator=
-  // return void to mute warning:
-  // implicit dereference will not access object of type
-  // ‘volatile {anonymous}::Bval’ in statement
+  Bval() : i(1) {}
+  Bval(const volatile Bval& r) : i(r.i) { a.i = r.a.i; }
+  Bval(const volatile Bval&& r) : i(r.i) { a.i = r.a.i; }
 
   void operator=(const volatile Bval& r) volatile {
     i   = r.i;
     a.i = r.a.i;
   }
-
-  void operator=(volatile Bval&& r) volatile {
-    i   = r.i;
-    a.i = r.a.i;
-  }
-
   void operator=(const volatile Bval&& r) volatile {
     i   = r.i;
     a.i = r.a.i;
   }
-
-  int i = 2;
-  A   a;
 };
-
-static void test_Bval() {
-#define TEST_VOLATILE_CTORS(ATYPE, OTYPE, ASSIGN) \
-  {                                               \
-    ATYPE a;                                      \
-    OTYPE b = a;                                  \
-    OTYPE c(a);                                   \
-    OTYPE d = std::move(a);                       \
-    OTYPE e(std::move(a));                        \
-    ASSIGN                                        \
-  }
-
-#define TEST_ASSIGN \
-  b = a;            \
-  c = std::move(a);
-
-#define NO_TEST_ASSIGN
-
-  using B   = Bval;
-  using CB  = const B;
-  using VB  = volatile B;
-  using CVB = const volatile B;
-
-  TEST_VOLATILE_CTORS(B, B, TEST_ASSIGN);
-  TEST_VOLATILE_CTORS(B, CB, NO_TEST_ASSIGN);
-  TEST_VOLATILE_CTORS(B, VB, TEST_ASSIGN);
-  TEST_VOLATILE_CTORS(B, CVB, NO_TEST_ASSIGN);
-
-  TEST_VOLATILE_CTORS(CB, B, TEST_ASSIGN);
-  TEST_VOLATILE_CTORS(CB, CB, NO_TEST_ASSIGN);
-  TEST_VOLATILE_CTORS(CB, VB, TEST_ASSIGN);
-  TEST_VOLATILE_CTORS(CB, CVB, NO_TEST_ASSIGN);
-
-  TEST_VOLATILE_CTORS(VB, B, TEST_ASSIGN);
-  TEST_VOLATILE_CTORS(VB, CB, NO_TEST_ASSIGN);
-  TEST_VOLATILE_CTORS(VB, VB, TEST_ASSIGN);
-  TEST_VOLATILE_CTORS(VB, CVB, NO_TEST_ASSIGN);
-
-  TEST_VOLATILE_CTORS(CVB, B, TEST_ASSIGN);
-  TEST_VOLATILE_CTORS(CVB, CB, NO_TEST_ASSIGN);
-  TEST_VOLATILE_CTORS(CVB, VB, TEST_ASSIGN);
-  TEST_VOLATILE_CTORS(CVB, CVB, NO_TEST_ASSIGN);
-
-#undef NO_TEST_ASSIGN
-#undef TEST_ASSIGN
-#undef TEST_VOLATILE_CTORS
-}
 
 TEST(nested_objects, volatile_outer) {
   peacalm::luaw l;
 
-  using B = Bval;
-
   l.register_member("i", &A::i);
-  l.register_member("i", &B::i);
-  l.register_member("a", &B::a);
+  l.register_member("i", &Bval::i);
+  l.register_member("a", &Bval::a);
 
-  l.register_member<A* const B::*>(
-      "aptr", [](auto* p) { return &(const_cast<B*>(p)->a); });
-  l.register_member<const A* const B::*>(
-      "acptr", [](auto* p) { return &(const_cast<B*>(p)->a); });
+  l.register_member<A* const Bval::*>(
+      "aptr", [](auto* p) { return &(const_cast<Bval*>(p)->a); });
+  l.register_member<const A* const Bval::*>(
+      "acptr", [](auto* p) { return &(const_cast<Bval*>(p)->a); });
 
-  l.register_member<std::shared_ptr<A> B::*>("aref", [](auto* p) {
-    return std::shared_ptr<A>(&const_cast<B*>(p)->a, [](...) {});
-  });
+  // Error: can not construct `volatile std::shared_ptr<A>`
+  // l.register_member<std::shared_ptr<A> Bval::*>("aref", [](auto* p) {
+  //   return std::shared_ptr<A>(&const_cast<Bval*>(p)->a, [](...) {});
+  // });
 
-  volatile B b;
+  volatile Bval b;
   b.a.i = 1;
-  b.i   = 2;
+  b.i   = 1;
   l.set("b", b);
 
   bool failed;
@@ -357,47 +393,41 @@ TEST(nested_objects, volatile_outer) {
   EXPECT_FALSE(failed);
 
 #if PEACALM_LUAW_SUPPORT_VOLATILE_OBJECT
-  EXPECT_EQ(bi, 2);
+  EXPECT_EQ(bi, 1);
 #else
   EXPECT_EQ(bi, 0);
 #endif
 
 #if PEACALM_LUAW_SUPPORT_VOLATILE_OBJECT
+  EXPECT_EQ(l.eval<int>("return b.i"), 1);
   EXPECT_EQ(l.dostring("b.i = 2"), LUA_OK);
-  EXPECT_EQ(l.eval<int>("return b.i"), 2);
-  EXPECT_EQ(l.eval<int>("return b.i"), 2);
-  EXPECT_EQ(l.eval<int>("return b.i"), 2);
   EXPECT_EQ(l.eval<int>("return b.i"), 2);
 
   EXPECT_EQ(l.eval<int>("return b.a.i"), 1);
   EXPECT_EQ(l.eval<int>("return b.aptr.i"), 1);
   EXPECT_EQ(l.eval<int>("return b.acptr.i"), 1);
-  EXPECT_EQ(l.eval<int>("return b.aref.i"), 1);
 
+  // can't modify b.a.i by b.a.i
+  EXPECT_EQ(l.dostring("b.a.i = 2"), LUA_OK);
+  EXPECT_EQ(l.eval<int>("return b.a.i"), 1);
+
+  // modify b.a.i by aptr
   EXPECT_EQ(l.dostring("b.aptr.i = 2"), LUA_OK);
   EXPECT_EQ(l.eval<int>("return b.a.i"), 2);
   EXPECT_EQ(l.eval<int>("return b.aptr.i"), 2);
   EXPECT_EQ(l.eval<int>("return b.acptr.i"), 2);
-  EXPECT_EQ(l.eval<int>("return b.aref.i"), 2);
 
-  EXPECT_EQ(l.dostring("b.aref.i = 3"), LUA_OK);
-  EXPECT_EQ(l.eval<int>("return b.a.i"), 3);
-  EXPECT_EQ(l.eval<int>("return b.aptr.i"), 3);
-  EXPECT_EQ(l.eval<int>("return b.acptr.i"), 3);
-  EXPECT_EQ(l.eval<int>("return b.aref.i"), 3);
 #else
   EXPECT_EQ(l.eval<int>("return b.a.i"), 0);
   EXPECT_EQ(l.eval<int>("return b.aptr.i"), 0);
   EXPECT_EQ(l.eval<int>("return b.acptr.i"), 0);
-  EXPECT_EQ(l.eval<int>("return b.aref.i"), 0);
+  // EXPECT_EQ(l.eval<int>("return b.aref.i"), 0);
 
   EXPECT_EQ(l.gettop(), 0);
 
   EXPECT_NE(l.dostring("b.i = 2"), LUA_OK);
   l.log_error_out();
   EXPECT_NE(l.dostring("b.aptr.i = 2"), LUA_OK);
-  l.log_error_out();
-  EXPECT_NE(l.dostring("b.aref.i = 3"), LUA_OK);
   l.log_error_out();
 
   EXPECT_EQ(l.gettop(), 0);
@@ -426,8 +456,8 @@ TEST(nested_objects, member_volatile) {
 
 #if PEACALM_LUAW_SUPPORT_VOLATILE_OBJECT
 
-  EXPECT_EQ(l.eval<int>("return c.b.i"), 2);
-  EXPECT_EQ(l.eval<int>("return c.bptr.i"), 2);
+  EXPECT_EQ(l.eval<int>("return c.b.i"), 1);
+  EXPECT_EQ(l.eval<int>("return c.bptr.i"), 1);
 
   EXPECT_EQ(l.eval<int>("return c.b.a.i"), 1);
   EXPECT_EQ(l.eval<int>("return c.bptr.a.i"), 1);
@@ -435,11 +465,10 @@ TEST(nested_objects, member_volatile) {
   EXPECT_EQ(l.eval<int>("return c.bptr.aptr.i"), 1);
 
   EXPECT_EQ(l.dostring("c.bptr.i = 3;"), LUA_OK);
-  EXPECT_EQ(l.dostring("c.bptr.aptr.i = 4;"), LUA_OK);
-
   EXPECT_EQ(l.eval<int>("return c.b.i"), 3);
   EXPECT_EQ(l.eval<int>("return c.bptr.i"), 3);
 
+  EXPECT_EQ(l.dostring("c.bptr.aptr.i = 4;"), LUA_OK);
   EXPECT_EQ(l.eval<int>("return c.b.a.i"), 4);
   EXPECT_EQ(l.eval<int>("return c.bptr.a.i"), 4);
   EXPECT_EQ(l.eval<int>("return c.b.aptr.i"), 4);
@@ -456,12 +485,6 @@ TEST(nested_objects, member_volatile) {
 #endif
 
   EXPECT_EQ(l.gettop(), 0);
-
-  auto the_PEACALM_LUAW_SUPPORT_VOLATILE_OBJECT =
-      PEACALM_LUAW_SUPPORT_VOLATILE_OBJECT;
-  watch(the_PEACALM_LUAW_SUPPORT_VOLATILE_OBJECT);
 }
-
-#endif
 
 }  // namespace
